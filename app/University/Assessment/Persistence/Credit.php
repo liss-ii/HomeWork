685280d043950aba258dd2f9a0718395f2ca4f3f
liss-ii <iryna.lisovenko@gmail.com>
2019-01-14 23:09:28 +0200

    New classes

---
+++ app/University/Assessment/Persistence/Credit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace University\Assessment\Persistence;
+
+class Credit extends \University\Services\Persistence\Resource
+{
+
+    /**
+     * @var string
+     */
+    protected $table_name = "credit";
+
+}
